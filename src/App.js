aae9b5549659e495efb2a182059b4731508a2af1
Raymond <raymondtan@berkeley.edu>
2021-06-27 16:25:03 -0700

    Updated header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./components/Pages/HomePage.js";
+import HowToPlay from "./components/Pages/HowToPlay.js";
+import Play from "./components/Pages/Play.js";
 import Header from "./components/Layout/Header.js";
 
 
 function App() {
   return (
-    <React.Fragment>
-      <Header />
-    </React.Fragment>
+    <BrowserRouter>
+      <Header /> 
+      <Switch>
+        <Route exact path="/" component={ HomePage }/>
+        <Route path="/howtoplay" component={ HowToPlay } />
+        <Route path="/play" component={ Play } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
